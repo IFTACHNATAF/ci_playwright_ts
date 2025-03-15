5c9ac68055603e550887e61d367b693b540a2afa
Iftach Nataf <iftachn85@gmail.com>
2025-03-15 13:05:28 +0200

    second commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test';
+
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
+
+
+// test('test', async ({ page }) => {
+//   await page.goto('http://uitestingplayground.com/');
+//   await page.getByRole('link', { name: 'Scrollbars' }).click();
+//   await page.getByRole('button', { name: 'Hiding Button' }).click();
+// });
+
+
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('button', { name: 'אוטומציה' }).click();
+  await page.getByRole('heading', { name: 'אוטומציה' }).click();
+  await page.getByRole('button', { name: 'פולסטאק' }).click();
+  await page.getByRole('heading', { name: 'פולסטאק' }).click();
+  await page.getByRole('button', { name: 'ייעוץ ותמיכה' }).click();
+  await page.getByRole('heading', { name: 'ייעוץ ותמיכה' }).click();
+  await page.getByRole('button', { name: 'אזור לקוחות' }).click();
+  await page.getByRole('checkbox').check();
+  await page.getByRole('button', { name: 'go-home-button' }).click();
+  await page.getByRole('button', { name: 'ראו עוד על RPA' }).click();
+  await page.getByRole('button', { name: 'כפתור-לקרוא-עוד' }).click();
+  await page.getByRole('button', { name: 'see-more-button' }).click();
+  await page.getByRole('textbox', { name: 'שם מלא' }).click();
+  await page.getByRole('textbox', { name: 'שם מלא' }).fill('test');
+  await page.getByRole('textbox', { name: 'כתובת מייל' }).click();
+  await page.getByRole('textbox', { name: 'כתובת מייל' }).fill('mail@mail.com');
+  await page.getByRole('textbox', { name: 'מספר טלפון' }).click();
+  await page.getByRole('textbox', { name: 'מספר טלפון' }).fill('0535555521');
+  await page.getByRole('textbox', { name: 'הודעה' }).click();
+  await page.getByRole('textbox', { name: 'הודעה' }).fill('yest');
+  await page.getByRole('button', { name: 'submit-contact-form' }).click();
+  await page.getByText('הטופס נשלח בהצלחה !').click();
+  await page.getByText('אצור קשר בהקדם').click();
+});
