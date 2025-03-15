cdc58c337029a113baf25736bf3ae114ece09170
Iftach Nataf <iftachn85@gmail.com>
2025-03-15 14:06:48 +0200

    set separate test for site functionality

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -25,19 +25,53 @@ import { test, expect } from '@playwright/test';
 // });
 
 
-test('test', async ({ page }) => {
+test('test navigate \"Automation\"', async ({ page }) => {
   await page.goto('/');
   await page.getByRole('button', { name: 'אוטומציה' }).click();
   await page.getByRole('heading', { name: 'אוטומציה' }).click();
+
+});
+
+
+test('test navigate \"Fullstack\"', async ({ page }) => {
+  await page.goto('/');
   await page.getByRole('button', { name: 'פולסטאק' }).click();
   await page.getByRole('heading', { name: 'פולסטאק' }).click();
+
+});
+
+test('test navigate \"Consult and support\"', async ({ page }) => {
+  await page.goto('/');
   await page.getByRole('button', { name: 'ייעוץ ותמיכה' }).click();
   await page.getByRole('heading', { name: 'ייעוץ ותמיכה' }).click();
+
+});
+
+test('test navigate \"Clien area\" and check show-password', async ({ page }) => {
+  await page.goto('/');
   await page.getByRole('button', { name: 'אזור לקוחות' }).click();
   await page.getByRole('checkbox').check();
+});
+
+test('test navigate \"Clien area\" and navigate back home page', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('button', { name: 'אזור לקוחות' }).click();
   await page.getByRole('button', { name: 'go-home-button' }).click();
+});
+
+
+test('test navigate \"Read more about RPA\" and expad exlpain ', async ({ page }) => {
+  await page.goto('/');
+
   await page.getByRole('button', { name: 'ראו עוד על RPA' }).click();
   await page.getByRole('button', { name: 'כפתור-לקרוא-עוד' }).click();
+
+});
+
+
+
+test('test fill contact with form and verify its send by click toast\'s', async ({ page }) => {
+  await page.goto('/');
   await page.getByRole('button', { name: 'see-more-button' }).click();
   await page.getByRole('textbox', { name: 'שם מלא' }).click();
   await page.getByRole('textbox', { name: 'שם מלא' }).fill('test');
